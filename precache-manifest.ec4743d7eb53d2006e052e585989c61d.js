c52487c5199bb3454f367c52643a0345c2e70a5f
Saurabh <saurabhadhau1@gmail.com>
2020-04-26 20:45:54 +0530

    Updates

---
+++ precache-manifest.ec4743d7eb53d2006e052e585989c61d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dfee81b7c5760116a78e50cdbd83254",
+    "url": "/To-Do-App/index.html"
+  },
+  {
+    "revision": "f53cf880d5861901c5dc",
+    "url": "/To-Do-App/static/css/main.20310d44.chunk.css"
+  },
+  {
+    "revision": "519a8572add06b54594e",
+    "url": "/To-Do-App/static/js/2.fa7e2d8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App/static/js/2.fa7e2d8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53cf880d5861901c5dc",
+    "url": "/To-Do-App/static/js/main.c149afc0.chunk.js"
+  },
+  {
+    "revision": "4b85f34772e8a7601f10",
+    "url": "/To-Do-App/static/js/runtime-main.ab756cab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
